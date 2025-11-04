b37b76007fe5b10a1cf999226b60de53bca88434
fincrypta <connor0macleod@gmail.com>
2025-11-04 19:07:01 +0300

    Update related-articles.js

--- js/related-articles.js
+++ js/related-articles.js
@@ -23,7 +23,8 @@ const allArticles = [
   { url: "/blog/articles/Dolgosrochnye_investitsii_v_BTC_i_ETH.html", title: "Долгосрочные инвестиции в BTC и ETH: стратегия HODL" },
   { url: "/blog/articles/Kak_zapusk_ETF_na_bitkoin_povliyal_na_rynok.html", title: "Как запуск ETF на биткоин повлиял на рынок" },
   { url: "/blog/articles/Sravnenie_pokupki_BTC_cherez_P2P_i_birzhu.html", title: "Сравнение покупки BTC через P2P и биржу" },
-  { url: "/blog/articles/TON_kak_kupit_i_gde_khranit_v_2025_godu.html", title: "TON: как купить и где хранить в 2025 году" }
+  { url: "/blog/articles/TON_kak_kupit_i_gde_khranit_v_2025_godu.html", title: "TON: как купить и где хранить в 2025 году" },
+  { url: "/blog/articles/Kak_kupit_kriptovalyutu_cherez_Tinkoff_Bank_v_2025_godu.html", title: "Как купить криптовалюту через Тинькофф Банк в 2025 году" }
 ];
 
 // Определяем текущую страницу
