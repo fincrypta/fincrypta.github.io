d0b9e3645521d4b135dfdd461df91ff9020bb19b
fincrypta <connor0macleod@gmail.com>
2025-12-04 19:42:13 +0300

    Update related-articles.js

--- js/related-articles.js
+++ js/related-articles.js
@@ -28,7 +28,7 @@ const allArticles = [
   { url: "/blog/articles/Kak_proyti_verifikatsiyu_na_OKX_v_2025_godu.html", title: "Как пройти верификацию на OKX в 2025 году" },
   { url: "/blog/articles/Metamask_poshagovaya_instruktsiya_po_nastroike.html", title: "Metamask: пошаговая инструкция по настройке" },
   { url: "/blog/articles/Kak_steikat_Ethereum_i_poluchat_dokhod.html", title: "Как стейкать Ethereum 2.0 и получать доход" },
-  { url: "/blog/articles/Kak_zarabotat_na_farmingе_likvidnosti.html", title: "Как заработать на фарминге ликвидности" },
+  { url: "/blog/articles/Kak_zarabotat_na_farminge_likvidnosti.html", title: "Как заработать на фарминге ликвидности" },
   { url: "/blog/articles/Nalogooblozhenie_dokhodov_ot_kriptovalyut.html", title: "Налогообложение доходов от криптовалют" },
   { url: "/blog/articles/Kak_sostavit_kriptoportfel_v_2025.html", title: "Как составить криптопортфель в 2025" },
   { url: "/blog/articles/Rossiya_i_kriptovalyuty_novye_zakony_2025_goda.html", title: "Россия и криптовалюты: новые законы 2025 года" },
