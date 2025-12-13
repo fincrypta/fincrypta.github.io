3ea0786c4e942552bb285af44f3aa80c79be251c
fincrypta <connor0macleod@gmail.com>
2025-12-13 14:14:25 +0300

    Update related-articles.js

--- js/related-articles.js
+++ js/related-articles.js
@@ -40,7 +40,8 @@ const allArticles = [
   { url: "/blog/articles/Obzor_pulov_dlya_mayninga_v_2025.html", title: "Обзор пулов для майнинга в 2025" },
   { url: "/blog/articles/Chto_budet_s_NFT_v_2026_godu.html", title: "Что будет с NFT в 2026 году?" },
   { url: "/blog/articles/Mogut_li_zablokirovat_schet_za_pokupku_BTC.html", title: "Могут ли заблокировать счет за покупку BTC?" },
-  { url: "/blog/articles/Top-5_strategiy_treydinga_kriptovalyut.html", title: "Топ-5 стратегий трейдинга криптовалют" }
+  { url: "/blog/articles/Top-5_strategiy_treydinga_kriptovalyut.html", title: "Топ-5 стратегий трейдинга криптовалют" },
+  { url: "/blog/articles/Rost_TON_kak_ekosistema_Telegram_menyaet_kriptorynok.html", title: "Рост TON: как экосистема Telegram меняет крипторынок" }
 ];
 
 // Определяем текущую страницу
